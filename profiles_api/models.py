2e506d8dcfed74ee04e2ad94b145dd01a7bd7f2b
ghigbie <gehigbie@gmail.com>
2019-07-23 20:50:11 -0600

    Edit spacing

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
+
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
@@ -10,25 +11,19 @@ class UserProfileManager(BaseUserManager):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
-
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
-
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
 
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
-
         user = self.cerate_user(email, name, password)
-
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
-
         return user
     
 
@@ -38,9 +33,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserProfileManager()
-
     USERNAME_FIELD = 'email' #This sets the email as required also.
     REQUIRED_FIELDS = ['name']
 

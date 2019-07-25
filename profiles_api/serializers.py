a016ff865da2afcd22a2b38d7f50d80d55304293
ghigbie <gehigbie@gmail.com>
2019-07-24 19:57:09 -0600

    Edit serializer

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,2 +1,6 @@
 from rest_framework import serializers
 
+class YoSerializer(serializers.Serializer):
+    """Serializes a name field for testing out APIView""" 
+    name = serializers.CharField(max_length=10)
+      

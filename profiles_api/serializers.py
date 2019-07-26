10acf2b5c670f7f7733d14b440b9515e2961e5bc
ghigbie <gehigbie@gmail.com>
2019-07-25 19:01:05 -0600

    Create a new serializer

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,23 @@
 from rest_framework import serializers
+from profiles_api import models
 
 class YoSerializer(serializers.Serializer):
     """Serializes a name field for testing out APIView""" 
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile"""
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {
+                    'input_type': 'password'
+                }
+            }
+        }
+
+        def create(self, validated_data):
+            """Create and return a new user"""
